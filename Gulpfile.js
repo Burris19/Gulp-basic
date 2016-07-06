13d8f614b41eb5286c1a2860b2558ab3f04964e5
Burris19 <julianhernandez19@gmail.com>
2016-07-06 11:09:25 -0600

    [add] minificar css

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,6 +1,8 @@
-var gulp    = require('gulp'),
-    sass    = require('gulp-sass'),
-    watch   = require('gulp-watch');
+var gulp        = require('gulp'),
+    sass        = require('gulp-sass'),
+    watch       = require('gulp-watch'),
+    sourcemaps  = require('gulp-sourcemaps'),
+    cssnano     = require('gulp-cssnano');
 
 var config = {
     scssDir: './assets/scss',
@@ -9,8 +11,11 @@ var config = {
 
 gulp.task('style', function(){
    gulp.src(config.scssDir + '/*.scss') // Indicamos que archivos queremos compilar
+   .pipe(sourcemaps.init())
    .pipe(sass())   
    .on('error', sass.logError) // 
+   .pipe(cssnano()) //minificamos el css antes de ser escrito
+   .pipe(sourcemaps.write('maps')) //
    .pipe(gulp.dest(config.cssDir)) // Indicamos donde queremos guardar los estilos
 });
 
