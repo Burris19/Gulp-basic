a303de14bc23ab70b31367281ead3ffb792207dd
Burris19 <julianhernandez19@gmail.com>
2016-07-06 11:34:27 -0600

    [add] parametros y condicionales

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,7 +2,17 @@ var gulp        = require('gulp'),
     sass        = require('gulp-sass'),
     watch       = require('gulp-watch'),
     sourcemaps  = require('gulp-sourcemaps'),
-    cssnano     = require('gulp-cssnano');
+    cssnano     = require('gulp-cssnano'),
+    argv        = require('yargs').argv,
+    gulpif      = require('gulp-if');
+
+var isProduction;
+
+if(argv.prod) {
+    isProduction = true;
+} else {
+    isProduction = false;
+}
 
 var config = {
     scssDir: './assets/scss',
@@ -14,14 +24,13 @@ gulp.task('style', function(){
    .pipe(sourcemaps.init())
    .pipe(sass())   
    .on('error', sass.logError) // 
-   .pipe(cssnano()) //minificamos el css antes de ser escrito
-   .pipe(sourcemaps.write('maps')) //
+   .pipe(gulpif(isProduction, cssnano(), sourcemaps.write('maps') )) //minificamos el css antes de ser escrito   
    .pipe(gulp.dest(config.cssDir)) // Indicamos donde queremos guardar los estilos
 });
 
 
 gulp.task('watch', function(){
     watch(config.scssDir + '/**/*.scss', function(){
-        gulp.start('style'); // indica a la funcion de wath que invoque la tarea que indicamos
+        gulp.start('style'); // indica a la funcion de wath que invoque la tarea que indicamos        
     });
 });
